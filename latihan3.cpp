71a5d625fb51a7ed379047ff2fecff85493bab6b
Basrisngji <basrisangaji23@gmail.com>
2021-11-04 17:13:34 +0700

    menambahkan latihan 3

---
+++ latihan3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+main(){
+    int a,b;
+    for (a=0; a<=9; a++){
+        for (b=0; b<=9; b++){
+            cout<<a+b;
+        }
+        cout<<endl;
+    }
+}
